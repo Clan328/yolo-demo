ecc94d73f0a8d7e92873d4f30306eaea928134d0
Clan328 <66789889+Clan328@users.noreply.github.com>
2021-08-12 22:02:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# yolo-demo
+ This YOLO v3 demo recognizes objects in images and videos.
---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from PIL import Image
+import tempfile
+
+import model
+
+st.title("YOLO Demo")
+st.header("This YOLO v3 demo recognizes objects in images and videos.")
+
+st.sidebar.markdown("This demo was made during the Inspirit AI course by [Hugo Dominguez](https://github.com/Clan328).")
+
+uploaded_file = st.file_uploader("Drag and drop any image or video to test the YOLO model", type=["jpg", "jpeg", "png", "mp4", "mov"])
+
+if uploaded_file is not None:
+  if uploaded_file.type.startswith("image"):
+    image = Image.open(uploaded_file)
+    st.image(model.detect_image(image))
+  else:
+    tfile = tempfile.NamedTemporaryFile(delete=False) 
+    tfile.write(uploaded_file.read())
+
+    path = tempfile.NamedTemporaryFile(suffix='.mp4', delete=False)
+
+    model.detect_video(tfile.name, path.name)
+
+    progress = st.empty()
+    progress.empty()
+
+    st.video(path.name)
