7e074308e4234c4d9bb70285dae4b975e4f4f320
Clan328 <66789889+Clan328@users.noreply.github.com>
2021-08-13 01:26:30 +0100

    Update

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import streamlit as st
 from PIL import Image
 import tempfile
 
-import model
+from model import detect_image, detect_video
+
+st.set_page_config(page_title="YOLO Demo", layout="wide")
 
 st.title("YOLO Demo")
 st.header("This YOLO v3 demo recognizes objects in images and videos.")
@@ -14,14 +16,14 @@ uploaded_file = st.file_uploader("Drag and drop any image or video to test the Y
 if uploaded_file is not None:
   if uploaded_file.type.startswith("image"):
     image = Image.open(uploaded_file)
-    st.image(model.detect_image(image))
+    st.image(detect_image(image))
   else:
     tfile = tempfile.NamedTemporaryFile(delete=False) 
     tfile.write(uploaded_file.read())
 
     path = tempfile.NamedTemporaryFile(suffix='.mp4', delete=False)
 
-    model.detect_video(tfile.name, path.name)
+    detect_video(tfile.name, path.name)
 
     progress = st.empty()
     progress.empty()
